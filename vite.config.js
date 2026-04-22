a6a99630370077b06f59064e8faeac1dc23bfd3f
vaani684 <guptavaani2003@gmail.com>
2026-04-22 23:45:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 1. Set the base path
+  // If your URL is https://vaani684.github.io/, use '/'
+  // If your URL is https://vaani684.github.io/portfolio/, use '/portfolio/'
+  base: '/', 
+  
+  build: {
+    // 2. Ensure the build output is clean
+    outDir: 'dist',
+  }
 })
